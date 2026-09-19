838ef9152ba09bacdee2c6a83d98294f14396f4a
Rudraksh Raina <1819rudraksh@gmail.com>
2026-09-20 00:28:58 +0530

    fix repository owner authentication

--- Backend/Controllers/repoController.js
+++ Backend/Controllers/repoController.js
@@ -6,7 +6,7 @@ const { recordContribution } = require("./contributionController");
 
 const createRepository = async (req, res) => {
     const {name, description, content, visibility, issues} = req.body;
-    const owner = localStorage.getItem("userId")
+    const owner = req.user.id
     try{
         if(!name){
             return res.status(400).json({error: "Repository name is required!"});
